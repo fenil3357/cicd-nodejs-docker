0c125a3b04ffccfc058286fd91041b91974e6aa7
fenil3357 <fenil3357@gmail.com>
2025-02-26 15:05:39 +0530

    change

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  return res.send('Welcome to the server Again !!');
+  return res.send('Welcome to the server!!');
+})
+
+app.get('/new', (req, res) => {
+  return res.send('This is new feature!')
 })
 
 app.listen(PORT, () => {
